77f498dcb75b25b326633502b33360ebf815e43f
Surya Teja <pratapreddy0808@gmail.com>
2023-06-12 02:52:09 +0530

    Initial Commit

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,9 +1,8 @@
 const allowedOrigins = [
   "https://www.yoursite.com",
-  // "http://127.0.0.1:5500",
+  "https://elegant-phoenix-b90329.netlify.app",
   "http://localhost:3500",
   "http://localhost:3000",
-  // "http://127.0.0.1:5500/index.html",
 ];
 
 module.exports = allowedOrigins;
